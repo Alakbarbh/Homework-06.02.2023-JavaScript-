155bafa147c191bed393dfc71d0be02f3cd65883
Alakbar B. Hasanov <alakbarbh@code.local>
2023-02-06 17:58:36 +0400

    Homework 06.02.2023 JavaScript intro

---
+++ Homework(06.02.2023 JavaScript intro)/script.js
@@ -0,0 +1,148 @@
+"use strict"
+
+// console.log("hello")
+
+// alert("salam Cavid")
+
+// var surname = "Ismayilzade"
+// let name = "Cavid"
+
+// console.log(surname + " " + name)
+
+
+
+// let age = 55;
+// if(true){
+//    let age = 55;
+//    console.log(age)
+// }
+
+
+
+// let age = 17;
+
+// let name = "Cavid"
+
+// let isMarried = false;
+
+// let address;
+
+// let phone = null;
+
+
+
+// console.log(typeof age)
+
+// console.log(typeof name)
+
+// console.log(typeof isMarried)
+
+// console.log(typeof address)
+
+// console.log(typeof phone)
+
+
+// if(phone == null){
+//     console.log("address null oldu")
+// }
+
+
+// let stu = {
+//     name:"Cavid",
+//     surname:"Ismayilzade"
+// }
+
+
+// console.log(stu.name + " " + stu.surname)
+
+// console.log(`${stu.name} ${stu.surname}`);
+
+
+// let students = ["Cavid","Pervin","Mirze",100,200,true]
+
+// console.log("Cavid"/3);
+
+// for(let i = 0; i < students.length;i ++){
+//     console.log(students[i])
+// }
+
+
+// let username = prompt("Please add your username")
+
+// let isOnline = confirm("Is he online?")
+
+// if(isOnline){
+//     console.log("He is online");
+// }else{
+//     console.log("He is offline");
+// }
+
+// debugger
+
+// console.log(isOnline)
+
+
+// let temp = 25;
+
+// if (temp < 10) {
+//     console.log("Normaldir")
+// } else if (temp > 20) {
+//     console.log("Istidir");
+// }else{
+//     console.log("Hec bir sey");
+// }
+
+
+// let count = 66;
+
+// switch (count) {
+//     case 5:
+//         console.log("besh");
+//         break;
+//     case 6:
+//         console.log("alti");
+//     default:
+//         console.log("not found");
+// }
+
+
+// let name;
+
+// for(let i = 0; i<=20;i++){
+//     console.log(i);
+// }
+
+
+// let result = 2**3;
+
+// console.log("44c"*2);
+
+
+// let isMarried = true;
+
+// let result = isMarried?"Evlidir":"Subaydir"
+
+// console.log(result);
+
+
+
+// let result = address??"Ehmedli";
+
+// console.log(result);
+
+// if(address == null){
+//     console.log("Ehmedli");
+// }else{
+//     console.log(address);
+// }
+
+
+let address = "";
+
+
+
+let res = address == null ? "Ehmedli":address;
+
+console.log(res)
+
+
